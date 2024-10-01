4ecd41417f8a22abd6ed73d2158f13fc7aba81d0
JohnnysGithubAccount <taifa2907@gmail.com>
2024-10-01 13:44:28 +0700

    reformatting the project
    Changing file names and clear up redundant comments

--- Web/static/app.js
+++ Web/static/app.js
@@ -1,6 +1,5 @@
 
 async function updateHumidity(sensorId, circleBorder) {
-//    const targetHumidity = Math.floor(Math.random() * 101); // giá trị mà sensor gửi lên
     try {
         const response = await fetch(`/get_values?sensorId=${sensorId}`);
         if (!response.ok) {
@@ -14,43 +13,34 @@ async function updateHumidity(sensorId, circleBorder) {
     } catch (error) {
         console.error('Error fetching humidity:', error);
     }
-//    animateHumidityChange(sensorId, targetHumidity);
 }
 
 function animateHumidityChange(sensorId, targetHumidity, circleBorder) {
-    let currentHumidity = parseInt(document.getElementById(`${sensorId}`).innerText) || 0; // Giá trị bắt đầu
-    const stepTime = 20; // Thay đổi thời gian giữa các bước (ms)
-    const totalSteps = Math.abs(targetHumidity - currentHumidity); // Tổng số bước cần thực hiện
+    let currentHumidity = parseInt(document.getElementById(`${sensorId}`).innerText) || 0; 
+    const stepTime = 20; 
+    const totalSteps = Math.abs(targetHumidity - currentHumidity); 
     let currentStep = 0;
 
     const interval = setInterval(() => {
         if (currentStep < totalSteps) {
             currentHumidity += (targetHumidity > currentHumidity ? 1 : -1);
-            const percentage = currentHumidity; // Giá trị độ ẩm từ 0 đến 100
+            const percentage = currentHumidity; 
             if (sensorId != "air-temperature") {
                 document.getElementById(`${sensorId}`).textContent = currentHumidity + '%';
                 document.getElementById(`${circleBorder}`).style.background = `conic-gradient(#4dd0e1 ${percentage * 3.6}deg, #e0e0e0 ${percentage * 3.6}deg)`;
             }
             else {
                 
-                    const maxTemperature = 80; // Nhiệt độ tối đa
-                    const minTemperature = 10; // Nhiệt độ tối thiểu
-                    // Lấy giá trị ngẫu nhiên để mô phỏng
-                    // const targetTemperature = Math.floor(Math.random() * (maxTemperature - minTemperature + 1)) + minTemperature; 
+                    const maxTemperature = 80; 
+                    const minTemperature = 10; 
                     const fill = document.getElementById('circleBorder3');
                     const temperatureText = document.getElementById('air-temperature');
                     
-                    temperatureText.textContent = targetHumidity + '°C'; // Hiển thị giá trị nhiệt độ
+                    temperatureText.textContent = targetHumidity + '°C'; 
 
-                    // Cập nhật chiều cao của phần màu đỏ
                     const heightPercentage = ((targetHumidity - minTemperature) / (maxTemperature - minTemperature)) * 100;
                     fill.style.height = `${heightPercentage}%`;
-            
-//                fix this, change this to a thermometer
-                // document.getElementById(`${sensorId}`).textContent = currentHumidity + ' degrees';
-                // document.getElementById(`${circleBorder}`).style.background = `conic-gradient(#4dd0e1 ${percentage * 3.6}deg, #e0e0e0 ${percentage * 3.6}deg)`;
             }
-
             currentStep++;
         } else {
             clearInterval(interval);
@@ -58,9 +48,6 @@ function animateHumidityChange(sensorId, targetHumidity, circleBorder) {
     }, stepTime);
 }
 
-
-
-// Cập nhật giá trị độ ẩm mỗi 4 giây cho từng cảm biến
 setInterval(() => updateHumidity("air-humidity", "circleBorder1"), 1000);
 setInterval(() => updateHumidity("dirt-humidity", "circleBorder2"), 1000);
 setInterval(() => updateHumidity("air-temperature", "circleBorder3"), 1000);
